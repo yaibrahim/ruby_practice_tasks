aa7b360c9908a15bdf1a3e09e2a05e4948e294f9
Ammar <mohammadammarkhan2@gmail.com>
2022-03-08 19:58:29 +0500

    continue

--- Task3.rb
+++ Task3.rb
@@ -1,33 +1,21 @@
-puts 'Enter Desired Length of password'
-length_of_password = gets.to_i
-puts 'Enter Length of alphabets less than or equal to 26'
-alpha_length = gets.to_i
-puts 'Enter Length of numerical less than or equal to 10'
-num_length = gets.to_i
-puts 'Enter Length of symbols less than or equal to 20'
-symbols_length = gets.to_i
+# Global Variables
 
-numeric = '0123456789'
-alphabets_small = 'abcdefghijklmnopqrstuvwxyz'
-alphabets_large = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-symbols = '!@#$%^&*/+-`~?><:;|.'
+$numeric = '0123456789'
+$alphabets_small = 'abcdefghijklmnopqrstuvwxyz'
+$alphabets_large = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+$symbols = '!@#$%^&*/+-`~?><:;|.'
 
-if alpha_length <= alphabets_large.length && alpha_length <= alphabets_small.length && num_length <= numeric.length && symbols_length <= symbols.length
+class PasswordGenerator
+  attr_accessor :user_pass, :alphabets_l, :numeric_l, :symbol_l
 
-  password_items = numeric[0, num_length] + alphabets_small[0, alpha_length] + alphabets_large[0, alpha_length] + symbols[0, symbols_length]
-  password = ''
-  index = 0
+  def initialize(user_length,alpha_length,num_length,sym_length)
+    @user_pass = user_length
+    @alphabets_l = alpha_length
+    @numeric_l = num_length
+    @symbol_l = sym_length
+  end
 
-  if length_of_password >= 7 && length_of_password <= password_items.length
-    while index < length_of_password
-      rand_value = rand 0..password_items.length - 1
-      password += password_items[rand_value]
-      index += 1
-    end
-
-    puts 'Random Generated Password:'
-    puts password.to_s != '' ? password : 'Unable to generate password'
-
+  def check_password(password)
     flag = false
     File.readlines('passwords1.csv').each { |line| flag = password == line }
 
@@ -38,11 +26,59 @@ if alpha_length <= alphabets_large.length && alpha_length <= alphabets_small.len
     pass_file.close
 
     puts 'Password Generated And save in file successfully'
-  else
-    puts 'The Length of Password should be greater than equal to 7'
   end
 
-else
-  puts "Check your input of numeric , alphabet and sysmbols length...
+  def random_password(index, size_of_password, password_items)
+    pass = ''
+    while index < size_of_password
+      rand_value = rand 0..password_items.length - 1
+      pass += password_items[rand_value]
+      index += 1
+    end
+    return pass
+  end
+
+  def generate_password()
+    check_alpha_large = alphabets_l <= $alphabets_large.length
+    check_alpha_small = alphabets_l<= $alphabets_small.length
+    check_numeric = numeric_l <= $numeric.length
+    check_sym = symbol_l <= $symbols.length
+
+    if check_alpha_large && check_alpha_small && check_numeric && check_sym
+
+      pass1 = $numeric[0, @numeric_l] + $alphabets_small[0, @alphabets_l]
+      pass2 = $alphabets_large[0, @alphabets_l] + $symbols[0, @symbol_l]
+
+      password_items = pass1 + pass2
+      password = ''
+      index = 0
+
+      if user_pass >= 7 && user_pass <= password_items.length
+        password = random_password(index, user_pass, password_items)
+
+        puts 'Random Generated Password:'
+        puts password.to_s != '' ? password : 'Unable to generate password'
+
+        check_password(password)
+      else
+        puts 'The Length of Password should be greater than equal to 7'
+      end
+
+    else
+      puts "Check your input of numeric , alphabet and sysmbols length...
    \n alphabets actual length is 26  \n Numerics are 10 \n symbols are 20"
+    end
+  end
 end
+
+puts 'Enter Desired Length of password'
+password_length = gets.to_i
+puts 'Enter Length of alphabets less than or equal to 26'
+aplphabets_length = gets.to_i
+puts 'Enter Length of numerical less than or equal to 10'
+numerics_length = gets.to_i
+puts 'Enter Length of symbols less than or equal to 20'
+symbols_length = gets.to_i
+
+pass = PasswordGenerator.new(password_length, aplphabets_length, numerics_length, symbols_length)
+pass.generate_password
